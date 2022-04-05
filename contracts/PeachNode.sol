d36202401ebc3471a2ec4289e7fea762c1413746
rayedsikder <rayedsikder33@gmail.com>
2022-04-06 05:01:54 +0600

    Import IERC20 interface

--- contracts/PeachNode.sol
+++ contracts/PeachNode.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract PeachNode is ERC1155, Ownable, ERC1155Supply {
     
